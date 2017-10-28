1e00b84f639e18bd3a3071a141a1c9943e59dba1
makstheimba <makstheimba@yandex.ru>
2017-10-28 12:12:48 +0300

    redo chart in svg

--- script.js
+++ script.js
@@ -7,17 +7,20 @@ const fetchGrossData = () => fetch(
 
 async function startApp() {
 	const grossData = await fetchGrossData();
+	const grossBarHeight = 100 / grossData.data.length;
+	const grossBarHeightPercentage = `${grossBarHeight}%`;
 	const grossValueScale = d3.scaleLinear()
-		.domain([0, d3.max(grossData.data.map(grossEntry => grossEntry[1]))])
+		.domain([0, d3.max(grossData.data, ([date, value]) => value)])
 		.range([0, 100])
 
 	d3.select('.chart')
-		.selectAll('div')
+		.selectAll('rect')
 		.data(grossData.data)
 			.enter()
-			.append('div')
-			.classed('chart-data', true)
-			.style('height', dataEntry => `${grossValueScale(dataEntry[1])}%`)
+			.append('rect')
+			.attr('width', ([date, value]) => `${grossValueScale(value)}%`)
+			.attr('height', grossBarHeightPercentage)
+			.attr('y', (entry, index) => `${index * grossBarHeight}%`)
 }
 
 window.onload = startApp;
