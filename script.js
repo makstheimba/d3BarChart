b3e56cbd0f4e234e75160b2bc4188afebcfe497a
U-MOSCOW\Maxim_Kulichenko <Maxim_Kulichenko@EPRUMOSL7691.moscow.epam.com>
2017-10-30 12:33:40 +0300

    Added tooltip for bars

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const fetchGrossData = () => fetch(
 )
 	.then(res => res.json())
 	.catch(alert);
-
+const getTooltipHTML = (date, value) => (
+	`<strong>${value} Billion $</strong><br />${date}`
+);
 async function startApp() {
 	const margin = {top: 10, left: 10, bottom: 20, right: 30};
 	const viewBoxWidth = 1000;
@@ -16,6 +18,8 @@ async function startApp() {
 		.attr('viewBox', `0 0 ${viewBoxWidth} ${viewBoxHeight}`)
 		.append('g')
 		.attr('transform', `translate(${margin.left}, ${margin.top})`);
+	const tooltip = d3.select('.tooltip');
+	const svgContainerNode = d3.select('.svg-container').node();
 	const grossData = await fetchGrossData();
 
 	yScale.domain([0, d3.max(grossData.data, ([date, value]) => value)]);
@@ -39,7 +43,15 @@ async function startApp() {
 	
 	bar.append('rect')
 		.attr('width', xScale.bandwidth())
-		.attr('height', ([date, value]) => height - yScale(value));
+		.attr('height', ([date, value]) => height - yScale(value))
+		.on('mousemove', ([date, value]) => {
+			const [left, top] = d3.mouse(svgContainerNode).map(val => `${val}px`);
+			
+			tooltip.classed('tooltip-hidden', false)
+				.html(getTooltipHTML(date, value))
+				.style('left', left)
+				.style('top', top);
+		}).on('mouseout', () => tooltip.classed('tooltip-hidden', true));
 	
 	chart.append('g')
 		.attr('transform', `translate(0, ${height})`)
